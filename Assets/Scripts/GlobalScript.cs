f5cb1c17d2d2538696e72ccb07746db28546beb5
nitvic793 <nithishvictor@gmail.com>
2016-01-30 01:06:53 +0530

    Basic shifting

---
+++ Assets/Scripts/GlobalScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalScript : MonoBehaviour {
+
+    public int currentWorldState = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            currentWorldState = (currentWorldState == 0) ? 1 : 0;
+        }
+	}
+}
---
+++ Assets/Scripts/ShiftBehaviorScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShiftBehaviorScript : MonoBehaviour {
+
+    public int state = 0;
+    public bool invisible = false;
+	// Use this for initialization
+	void Start () {
+	    if(state == 0)
+        {
+            invisible = false;
+        }
+        else if(state == 1)
+        {
+            invisible = true;
+        }
+        GetComponentInChildren<SpriteRenderer>().enabled = invisible;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int worldState = GameObject.FindObjectOfType<GlobalScript>().currentWorldState;
+        if(worldState==state)
+        {
+            GetComponentInChildren<SpriteRenderer>().enabled = true;
+            GetComponentInChildren<Collider2D>().enabled = true;
+        }
+        else
+        {
+            GetComponentInChildren<SpriteRenderer>().enabled = false;
+            GetComponentInChildren<Collider2D>().enabled = false;
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("Collision");
+    }
+}
