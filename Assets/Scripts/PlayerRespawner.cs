07ec68ef08c09c8a410b41aeb3fa6225a01c5cb7
Karthik Balakrishnan <karthikb351@gmail.com>
2016-01-30 14:42:00 +0530

    platform demo with ball

---
+++ Assets/Scripts/PlayerRespawner.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityStandardAssets._2D
+{
+	public class PlayerRespawner : MonoBehaviour
+	{
+		public Transform playerSpawn;
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if(other.tag=="Player")
+				other.transform.position = playerSpawn.position;
+		}
+	}
+}
