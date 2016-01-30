694179cc476873b2f8d3f290e7d12596a436ddc7
Karthik Balakrishnan <karthikb351@gmail.com>
2016-01-30 19:24:36 +0530

    menu init

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+
+	public void startLevel() {
+		Application.LoadLevel(1);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
