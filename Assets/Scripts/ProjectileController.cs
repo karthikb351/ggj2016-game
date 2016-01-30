0a1eb77f06f46b9171c043445a2dde21858f58f2
nitvic793 <nithishvictor@gmail.com>
2016-01-31 03:50:46 +0530

    Level 3 texturing

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -8,6 +8,7 @@ public class ProjectileController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+        GetComponent<SpriteRenderer>().sortingOrder = 1;
 	}
 	
 	// Update is called once per frame
