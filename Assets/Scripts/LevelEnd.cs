79363f6e3dfd4a47b08caeeb616e46ec738a657b
Karthik Balakrishnan <karthikb351@gmail.com>
2016-01-30 19:38:43 +0530

    Main Menu, basic level progression

---
+++ Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityStandardAssets._2D
+{
+	public class LevelEnd : MonoBehaviour
+	{
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.tag == "Ball") {
+				Application.LoadLevel (2);
+			}
+		}
+	}
+}
